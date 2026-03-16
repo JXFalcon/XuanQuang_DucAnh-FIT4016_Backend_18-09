37438c03edeb4f817bdacc3407acdcd7a4a17328
Nguyễn Xuân Quang <quanquan291024@gmail.com>
2026-03-16 12:06:22 +0700

    update 1

--- SportsTournamentManager/obj/Debug/net9.0/cailonma.AssemblyInfo.cs
+++ SportsTournamentManager/obj/Debug/net9.0/cailonma.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cailonma")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86cbbf0219eebfd55eaa3e0fa9f606ccb4eff90d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70acec9f27a85bf54b3f05e176cdc7d95818d2b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("cailonma")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cailonma")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
